61028e002341fb51cb99ddc1fd3f48a4c2f4b5bd
hk1996yhx <377020945@qq.com>
2024-01-30 02:54:03 +0800

    Site updated: 2024-01-30 02:54:01

--- zhheo/random.js
+++ zhheo/random.js
@@ -1,1 +1,1 @@
-var posts=["2024/01/30/hello-world/","2024/01/30/hello-world - 副本/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2024/01/30/hello-world - 副本/","2024/01/30/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
